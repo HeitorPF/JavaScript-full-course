9c12d6091188d97af64a67ec53dfd769fa5a8b37
HeitorPF <heitorpf12.hf@gmail.com>
2025-05-06 15:43:34 -0300

    first chapter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # JavaScript-full -course
 
+from [SuperSimpleDev video](https://youtu.be/EerdGm-ehJQ?si=hrT7Q0EWsO7v7hiN) on youtube
---
+++ basics/script.js
@@ -0,0 +1,39 @@
+//1a Use alert(...); to display 'Good morning' in a popup
+function l1a() {
+    alert('Good Morning')
+}
+
+function l1b() {
+    alert('Heitor')
+}
+
+function l1c() {
+    console.log(10 + 5)
+}
+
+function l1d() {
+    console.log(20 - 5)
+}
+
+function l1e() {
+    console.log(2 + 2 - 5)
+}
+
+function l1f() {
+    document.body.innerHTML = 'Good morning!'
+}
+
+function l1g() {
+    document.body.innerHTML = 'Heitor'
+}
+
+function l1h() {
+    console.log(10 + 8 + 20)
+}
+
+function l1i() {
+    console.log(100 - 20 - 50 + 200)
+}
+function l1j() {
+    document.body.innerHTML = ''
+}
