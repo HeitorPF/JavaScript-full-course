90fbcdff9b08dbaa6b7561339f81d843e1e1c091
HeitorPF <heitorpf12.hf@gmail.com>
2025-05-20 01:35:07 -0300

    update 11n
    código mais legivel e menos complexo na resolução

--- 11-arrays and loops/11n.js
+++ 11-arrays and loops/11n.js
@@ -1,5 +1,11 @@
 function countWords(words) {
   const obj = {}
+
+  for(let i = 0; i < words.length; i++) { // menor e menos complexo
+    obj[words[i]] ? obj[words[i]] ++ : obj[words[i]] = 1
+  }
+
+  /* código que eu fiz (maior em código e mais complexo)
   count = 0
   for(let x = 0; x < words.length; x++) {
     count = 0
@@ -10,6 +16,7 @@ function countWords(words) {
       }
     }
   }
+  */
   
   return obj
 }
