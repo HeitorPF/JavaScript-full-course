5419b1f4a7b293803f9a34838d7399c6f1b3db82
HeitorPF <heitorpf12.hf@gmail.com>
2025-05-23 18:24:07 -0300

    make button add to caer interactive
    show the cart in console with productsId

--- amazon-project/scripts/amazon.js
+++ amazon-project/scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-  {
-    img: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090
-  },
-  {
-    img: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095
-  },
-  {
-    img: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg"',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799
-  }
-]
-
 let productsHTML = ''
 
 products.forEach((product) => {
@@ -35,7 +5,7 @@ products.forEach((product) => {
   <div class="product-container">
     <div class="product-image-container">
       <img class="product-image"
-        src="${product.img}">
+        src="${product.image}">
     </div>
 
     <div class="product-name limit-text-to-2-lines">
@@ -76,7 +46,7 @@ products.forEach((product) => {
       Added
     </div>
 
-    <button class="add-to-cart-button button-primary">
+    <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${product.id}">
       Add to Cart
     </button>
   </div>
@@ -84,3 +54,27 @@ products.forEach((product) => {
 })
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML
+
+document.querySelectorAll('.js-add-to-cart-button').forEach((buttonElement) => {
+  buttonElement.addEventListener('click', () => {
+    const productId =  buttonElement.dataset.productId
+
+    let matchingItem
+    cart.forEach((item) => {
+      if(productId === item.productId) {
+        matchingItem = item
+      }
+    })
+
+    if(matchingItem) {
+      matchingItem.quantity += 1
+    }
+    else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      })
+    }
+    console.log(cart)
+  })
+})
