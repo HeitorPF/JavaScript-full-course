272c3021abb12a908c6ffbb8c9e65db5d8b45d1a
HeitorPF <heitorpf12.hf@gmail.com>
2025-06-11 16:27:00 -0300

    making requests to backend

--- amazon-project/scripts/checkout.js
+++ amazon-project/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../../18-backend/backend-practice.js'
 // import '../data/cart-class.js';
 
 renderOrderSummary()
