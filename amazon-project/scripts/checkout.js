b8189c06ea210c0a53bc7356158b8800e460a3b1
HeitorPF <heitorpf12.hf@gmail.com>
2025-06-11 16:49:03 -0300

    load products from bakend

--- amazon-project/scripts/checkout.js
+++ amazon-project/scripts/checkout.js
@@ -1,9 +1,12 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../../18-backend/backend-practice.js'
+import { loadProducts } from "../data/products.js";
+// import '../../18-backend/backend-practice.js'
 // import '../data/cart-class.js';
 
-renderOrderSummary()
-renderPaymentSummary()
-renderCheckoutHeader()
+loadProducts(() => {
+  renderOrderSummary()
+  renderPaymentSummary()
+  renderCheckoutHeader()
+})
