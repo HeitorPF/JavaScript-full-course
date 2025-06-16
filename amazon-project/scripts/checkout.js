bafceec7950c210b6b87f96d5e077f4b6941f4e6
HeitorPF <heitorpf12.hf@gmail.com>
2025-06-16 01:40:11 -0300

    promises and fetch

--- amazon-project/scripts/checkout.js
+++ amazon-project/scripts/checkout.js
@@ -1,12 +1,34 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProductsFetch } from "../data/products.js";
 // import '../../18-backend/backend-practice.js'
 // import '../data/cart-class.js';
 
-loadProducts(() => {
+Promise.all([
+  loadProductsFetch(),
+
+]).then(() => {
   renderOrderSummary()
   renderPaymentSummary()
   renderCheckoutHeader()
 })
+
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve()
+//   })
+
+// }).then(() => {
+//   renderOrderSummary()
+//   renderPaymentSummary()
+//   renderCheckoutHeader()
+// })
+
+
+// loadProducts(() => {
+  // renderOrderSummary()
+  // renderPaymentSummary()
+  // renderCheckoutHeader()
+// })
